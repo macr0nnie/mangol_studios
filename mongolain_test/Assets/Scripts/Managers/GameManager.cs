d0fcc5867af14844309fd33e62037af70ff6d59e
macr0nnie <macr0nnieasmr@gmail.com>
2025-02-19 15:58:44 -0500

    Created a singleton_class

--- mongolain_test/Assets/Scripts/Managers/GameManager.cs
+++ mongolain_test/Assets/Scripts/Managers/GameManager.cs
@@ -2,8 +2,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public DialogueUIController dui; 
+    public static GameManager Instance { get; private set; }
+    public DialogueUIController DUI; //thats my dui
+
+    //for the future
+    //public GameSettingsSO gameSettings; // Reference to game settings
+    //public PlayerDataSO playerData; // Reference to player data
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+     private void Awake()
+    {
+        // Implement Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+   
+      
+   
     void Start()
     {
         
