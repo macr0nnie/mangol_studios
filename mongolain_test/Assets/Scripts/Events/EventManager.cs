de92715c39f304cb1743cab0b4f9761d4e8903b5
macronnie <macr0nnieasmr@gmail.com>
2025-05-07 11:28:38 -0400

    added a commonent with refactoring to-do

--- mongolain_test/Assets/Scripts/Events/EventManager.cs
+++ mongolain_test/Assets/Scripts/Events/EventManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 
 public class EventManager : MonoBehaviour
 {
+    //to-do 
+    //refactoring update the current system with the events that we plan on using in the game 
     private static Dictionary<GameEvent, Delegate> eventDictionary = new();
 
     public static void Subscribe<T>(GameEvent eventType, Action<T> listener)
@@ -32,7 +34,6 @@ public class EventManager : MonoBehaviour
             Action<T> action = thisEvent as Action<T>;
             action?.Invoke(eventData);
         }
-
         // If there's a Unity Scriptable Object Event linked, trigger it too
         gameEventSO?.RaiseEvent();
     }
@@ -40,6 +41,7 @@ public class EventManager : MonoBehaviour
 
 public enum GameEvent
 {
+    //update the enums with the events we have decided on
     ExampleEvent,
     PlayerDeath,
     PlayerRespawn,
@@ -48,7 +50,6 @@ public enum GameEvent
     Quest,
     QuestCompleted
 }
-
 
 [CreateAssetMenu(fileName = "New Game Event", menuName = "Game Events/Event")]
 public class GameEventSO : ScriptableObject
