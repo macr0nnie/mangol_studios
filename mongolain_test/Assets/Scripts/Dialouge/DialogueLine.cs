e73c2792bbfe39eeec73b50a193a25514437ed91
macronnie <macr0nnieasmr@gmail.com>
2025-05-16 14:22:56 -0400

    cleaning up more code seperating the bubble script

--- mongolain_test/Assets/Scripts/Dialouge/DialogueLine.cs
+++ mongolain_test/Assets/Scripts/Dialouge/DialogueLine.cs
@@ -6,8 +6,8 @@ public class DialogueLine : ScriptableObject
 {
     [TextArea(3, 10)]
     public string dialogueText;                // The dialogue text
-    //public AudioClip voiceLine;                // Optional voice clip for this line
     public DialogueDisplayType displayType;    // How to display this line (Standard or Cutscene)
     public Sprite image;                       // Image for cutscene display (e.g., portrait or background)
     public List<DialogueChoice> choices = new List<DialogueChoice>();  // Branching choices, if any
 }
+ 
